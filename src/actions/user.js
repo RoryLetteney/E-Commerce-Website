cb5195d55c11fbba6ebd92946730c3a252016251
Rory Letteney <rory.letteney@gmail.com>
2018-10-15 20:50:16 -0500

    Finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -7,15 +7,89 @@ export function fetchUserPurchases() {
         type: SET_USER_PURCHASES,
         payload: [
             {
-                _id: 0,
-                title: "purchase 1",
-                amount: 8.02
+                _id: 1,
+                total: 19.40,
+                orderNumber: 'A004823854',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Rory Letteney',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
             },
             {
-                _id: 1,
-                title: "purchase 2",
-                amount: 19.40
-            }
+                _id: 2,
+                total: 19.40,
+                orderNumber: 'sadgdfhreg42',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
+            {
+                _id: 3,
+                total: 19.40,
+                orderNumber: 'dasgfdfghdagds',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Max Nelson',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
+            {
+                _id: 4,
+                total: 19.40,
+                orderNumber: 'agdagdsga',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Aaron Rodgers',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
+            {
+                _id: 5,
+                total: 19.40,
+                orderNumber: 'agdsaghffdhfdahgadf',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Julio Jones',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
+            {
+                _id: 6,
+                total: 19.40,
+                orderNumber: 'A004823854',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Rory Letteney',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
+            {
+                _id: 7,
+                total: 19.40,
+                orderNumber: 'A004823854',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Rory Letteney',
+                    shippingAddress: '1234 West State Street'
+                    
+                }
+            },
         ]
     })
 }
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PageTitle from '../pageTitle';
-import Purchases from './purchases';
-import PurchaseDetail from './purchaseDetail';
+import PageTitle from "../pageTitle";
+import Purchases from "./purchases";
+import PurchaseDetail from "./purchaseDetail";
 
 class PurchaseHistory extends Component {
-   render() {
-       return (
-           <div className="purchase-history">
-               <PageTitle className="purchase-history__page-title" title="Purchase History" />
-               <Purchases className="purchase-history__purchases"/>
-               <PurchaseDetail className="purchase-history__details"/>
-               <div className="purchase-history__bottom-border"></div>
-           </div>
-       )
-   }
+  render() {
+    return (
+      <div className="purchase-history">
+        <PageTitle
+          className="purchase-history__page-title"
+          title="Purchase History"
+        />
+        <div className="purchase-history__content">
+          <Purchases className="purchase-history__purchases" />
+          <PurchaseDetail className="purchase-history__details" />
+        </div>
+        <div className="purchase-history__bottom-border" />
+      </div>
+    );
+  }
 }
 
 export default PurchaseHistory;
