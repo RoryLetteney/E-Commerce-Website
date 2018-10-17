950c65d768126d80b49293a12dc24af23a049b79
Rory Letteney <rory.letteney@gmail.com>
2018-10-17 07:12:19 -0500

    Finished filtered products feature

---
+++ src/actions/shop.js
@@ -0,0 +1,120 @@
+import {
+    SET_SHOP_CATEGORIES,
+    SET_NAVBAR_LINKS,
+    SET_SHOP_PRODUCTS,
+    FILTER_PRODUCTS_WITH_CATEGORY_ID
+} from "./types";
+
+export function filterProductsWithCategoryId(_id) {
+    return ({
+        type: FILTER_PRODUCTS_WITH_CATEGORY_ID,
+        payload: _id
+    })
+}
+
+export function fetchShopCategories() {
+    return ({
+        type: SET_SHOP_CATEGORIES,
+        payload: [
+            {
+                _id: 0,
+                title: 'All',
+                active: true
+            },
+            {
+                _id: 1,
+                title: 'JavaScript',
+                active: false
+            },
+            {
+                _id: 2,
+                title: 'UI/UX',
+                active: false
+            },
+            {
+                _id: 3,
+                title: 'Linux',
+                active: false
+            },
+            {
+                _id: 4,
+                title: 'Python',
+                active: false
+            },
+            {
+                _id: 5,
+                title: 'UML',
+                active: false
+            },
+            {
+                _id: 6,
+                title: 'Ruby',
+                active: false
+            }
+        ]
+    })
+}
+
+export function fetchShopProducts() {
+    return ({
+        type: SET_SHOP_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                title: 'JavaScript in the Browser',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 1,
+                title: 'Graph Database',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 6]
+            },
+            {
+                _id: 2,
+                title: 'Full Stack Development',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 1, 4]
+            },
+            {
+                _id: 3,
+                title: 'User Interface Design',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 4,
+                title: 'JavaScript Development',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 5,
+                title: 'User Experience Design',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 6,
+                title: 'Advanced OOP',
+                description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+                price: 1.99,
+                quantity: 1,
+                belongsTo: [0, 6]
+            }
+        ]
+    })
+}
