b22568fb7846e2672cb9270f418dc20d24c0cda1
Rory Letteney <rory.letteney@gmail.com>
2018-10-19 12:30:49 -0500

    Removed the word browser from shop.js and user.js in actions

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -68,7 +68,7 @@ export function fetchShopProducts() {
         payload: [
             {
                 _id: 0,
-                title: 'JavaScript in the Browser',
+                title: 'JavaScript in the',
                 description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
                 price: 1.99,
                 quantity: 1,
